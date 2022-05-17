3c4fdb7f2cb1cb0e2e9c9d05ed042ff27185661a
Aamir <djzaamir@gmail.com>
2022-05-17 11:40:43 +0500

    Initialized RUST array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Basic Implementation of Bubble Sort");
+
+    const arr_size : usize = 10;
+    let mut arr : [i32; arr_size] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+   
+
+    
 }
