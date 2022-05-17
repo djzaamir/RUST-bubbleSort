8e7f490da089b9292b4840107a006de302b62e80
Aamir <djzaamir@gmail.com>
2022-05-17 11:48:35 +0500

    Implemented basic bubble sort algorithm in RUST

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
+const ARR_SIZE : usize = 10;
+
 fn main() {
     println!("Basic Implementation of Bubble Sort");
 
-    const arr_size : usize = 10;
-    let mut arr : [i32; arr_size] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    let mut arr : [i32; ARR_SIZE] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-   
+    print!("Before bubble sort -> ");
+    println!("{:?}", arr);
+
+    bubble_sort(&mut arr);
+
+    print!("After bubble sort -> ");
+    println!("{:?}", arr);
 
     
+}
+
+//Sort values in ascending order
+fn bubble_sort(arr : &mut [i32; ARR_SIZE]){
+
+    for i in 0..ARR_SIZE {
+        for j in (i+1)..ARR_SIZE {
+            
+            if arr[j] < arr[i]{
+                let temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            } 
+        }
+    }
 }
